c8be2d4504caaa902ac50f63d75a38800b54aa25
Miguel Velez <miguelvelez@mijecu25.com>
2021-02-04 15:51:33 -0500

    added var to pass program arguemnts

--- scripts/tracer.sh
+++ scripts/tracer.sh
@@ -2,8 +2,9 @@
 
 CP=../lib/utilities-1.2.10.jar:$1
 PROGRAM=$2
+ARG1=$3
 
 java -Xbootclasspath/a:../lib/utilities-1.2.10.jar:"$HOME"/.m2/repository/org/ow2/asm/asm-all/5.0.3/asm-all-5.0.3.jar \
   -javaagent:../assembly/tracer.jar=tracefile:test.trace \
   -cp "$CP" \
-  "$PROGRAM"
+  "$PROGRAM" "$ARG1"
